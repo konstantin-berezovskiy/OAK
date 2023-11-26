ef787a6d1cf5feae8e26eab799775919c8214ccb
anastasiia29d <87985573+anastasiia29d@users.noreply.github.com>
2023-11-26 14:32:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var dropdownBtn = document.getElementById("dropdown");
-    var dropdownContent = document.getElementById("drop");
-
-    dropdownBtn.addEventListener("click", function () {
-        dropdownContent.classList.toggle("show");
-    });
-
-    window.addEventListener("click", function (event) {
-        if (!event.target.matches("#dropdown")) {
-            var dropdowns = document.getElementsByClassName("dropdown-list");
-            for (var i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains("show")) {
-                    openDropdown.classList.remove("show");
-                }
-            }
-        }
+const navbarMenu = document.querySelector(".navbar .links");
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+const hideMenuBtn = navbarMenu.querySelector(".close-btn");
+const showPopupBtn = document.querySelector(".login-btn");
+const formPopup = document.querySelector(".form-popup");
+const hidePopupBtn = formPopup.querySelector(".close-btn");
+const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
+// Show mobile menu
+hamburgerBtn.addEventListener("click", () => {
+    navbarMenu.classList.toggle("show-menu");
+});
+// Hide mobile menu
+hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
+// Show login popup
+showPopupBtn.addEventListener("click", () => {
+    document.body.classList.toggle("show-popup");
+});
+// Hide login popup
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+// Show or hide signup form
+signupLoginLink.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
     });
 });
-function cross(x) {
-    x.classList.toggle("change");
-  }
